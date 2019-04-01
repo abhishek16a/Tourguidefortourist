5305fe741e0e05513c31ab296e877fb317dd99ca
abhishek16a <abhishekadhikari16a@gmail.com>
2019-04-01 11:41:49 +0545

    adding image using radiobutton

--- app/src/main/java/com/example/tourguidefortourist/MainActivity.java
+++ app/src/main/java/com/example/tourguidefortourist/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.tourguidefortourist;
 
+import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private RadioButton rdKajal;
+    private RadioButton rdHrithik;
+    private RadioButton rdPradeep;
+    private ImageView imgActor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rdKajal=findViewById(R.id.rdKajal);
+        rdHrithik=findViewById(R.id.rdHrithik);
+        rdPradeep=findViewById(R.id.rdPradeep);
+        imgActor=findViewById(R.id.imgActor);
+
+        rdKajal.setOnClickListener(this);
+        rdHrithik.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId()){
+            case R.id.rdKajal:
+                imgActor.setImageResource(R.drawable.kajal);
+//                Toast toast= Toast.makeText(this, "your message", Toast.LENGTH_LONG);
+//                toast.show();
+                break;
+            case R.id.rdHrithik:
+                imgActor.setImageResource(R.drawable.hrithik);
+                break;
+            case R.id.rdPradeep:
+                break;
+
+
+        }
     }
 }
